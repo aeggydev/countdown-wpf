93b8fa9677319798c3ac5ddf4739922478822655
aeggydev <kucernak.a@gmail.com>
2022-03-11 15:35:49 +0100

    Add helper methods and limits

--- countdown/Model.cs
+++ countdown/Model.cs
@@ -8,12 +8,37 @@ public class UpDown
 
 public class DoubleUpDown
 {
-    public string FirstDigit { get; set; } = new("0");
-    public string SecondDigit { get; set; } = new("0");
+    public DoubleUpDown(int limit)
+    {
+        Limit = limit;
+    }
+    public int Limit { get; init; }
+    public bool SetNumber(int number)
+    {
+        var asString = number.ToString();
+        if (number >= Limit)
+        {
+            return false;
+        }
+        if (asString.Length > 2)
+        {
+            return false;
+        }
+
+        if (asString.Length == 1)
+        {
+            asString = asString.PadLeft(2, '0');
+        }
+
+        (FirstDigit, SecondDigit) = (asString[0], asString[1]);
+        return true;
+    }
+    public char FirstDigit { get; set; } = '0';
+    public char SecondDigit { get; set; } = '0';
 }
 public class Model
 {
-    public DoubleUpDown Hours { get; set; } = new();
-    public DoubleUpDown Minutes { get; set; } = new();
-    public DoubleUpDown Seconds { get; set; } = new();
+    public DoubleUpDown Hours { get; set; } = new(100);
+    public DoubleUpDown Minutes { get; set; } = new(60);
+    public DoubleUpDown Seconds { get; set; } = new(60);
 }
