9cf63285bc133cca8c12a6878d259baec771ff2f
aeggydev <kucernak.a@gmail.com>
2022-03-11 14:52:13 +0100

    Add basic model

---
+++ countdown/Model.cs
@@ -0,0 +1,8 @@
+﻿namespace countdown;
+
+public class Model
+{
+    public (string, string) FirstUpDown { get; set; } = ("0,", "0");
+    public (string, string) SecondUpDown { get; set; } = ("0,", "0");
+    public (string, string) ThirdUpDown { get; set; } = ("0,", "0");
+}
