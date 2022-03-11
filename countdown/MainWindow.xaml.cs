7e0f27295273d78c7f867887444381de6437ca30
aeggydev <kucernak.a@gmail.com>
2022-03-11 14:32:19 +0100

    Initial commit

---
+++ countdown/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace countdown;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var box = (TextBox)sender;
+        if (Regex.IsMatch(box.Text, "[^0-9]"))
+        {
+            box.Text = "0";
+            TextBox_GotFocus(sender, null);
+        }
+    }
+
+    private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        var box = (TextBox)sender;
+        box.Dispatcher.BeginInvoke(new Action(() => box.SelectAll()));
+    }
+
+    private void ButtonStart_OnClick(object sender, RoutedEventArgs e)
+    {
+    }
+}
