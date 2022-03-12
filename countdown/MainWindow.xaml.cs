9bb29e2b95b199c346a5ddd10f0340695c49c4ea
aeggydev <kucernak.a@gmail.com>
2022-03-12 11:57:41 +0100

    Clean up MainWindow.xaml.cs

--- countdown/MainWindow.xaml.cs
+++ countdown/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Text.RegularExpressions;
-using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace countdown;
 
@@ -11,35 +8,10 @@ namespace countdown;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private ViewModel? ViewModel => DataContext as ViewModel;
     public MainWindow()
     {
         InitializeComponent();
-        var timer = new System.Threading.Timer(_ => ViewModel?.OnPropertyChange(nameof(ViewModel.SecondView)),
-            null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
     }
 
-    private Timer Timer { get; set; } = new Timer();
-    
-    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        var box = (TextBox)sender;
-        if (Regex.IsMatch(box.Text, "[^0-9]"))
-        {
-            box.Text = "0";
-            TextBox_GotFocus(sender, null);
-        }
-    }
-
-    private void TextBox_GotFocus(object sender, RoutedEventArgs e)
-    {
-        ViewModel?.OnPropertyChange(nameof(ViewModel.SecondView));
-        var box = (TextBox)sender;
-        box.Dispatcher.BeginInvoke(new Action(() => box.SelectAll()));
-    }
-
-    private void ButtonStart_OnClick(object sender, RoutedEventArgs e)
-    {
-        ViewModel?.GetTimer();
-    }
+    private ViewModel? ViewModel => DataContext as ViewModel;
 }
